8a19c24fb3a1291dca35fabb292ed6873e8574dd
Your Name <your.email@example.com>
2025-09-15 19:02:10 +0100

    Fix VSCode configuration for 24.11 compatibility - remove profiles option

--- modules/tools/vscode.nix
+++ modules/tools/vscode.nix
@@ -4,46 +4,44 @@
   programs.vscode = {
     enable = true;
     package = pkgs.vscode-fhs;
-    profiles.default = {
-      userSettings = {
-        "workbench.startupEditor" = "none";
-        "terminal.integrated.defaultProfile.linux" = "bash";
-        "terminal.integrated.env.linux" = {
-          "KUBECONFIG" = "/home/user/.kube/config";
-        };
-        "keyboard.dispatch" = "keyCode";
-        "vim.useSystemClipboard" = true;
-        "editor.lineNumbers" = "relative";
-        "vim.hlsearch" = true;
-        "vim.insertModeKeyBindings" = [];
-        "vim.normalModeKeyBindings" = [];
-        "vim.visualModeKeyBindings" = [];
-        "settingsSync.ignoredSettings" = [];
-        "settingsSync.ignoredExtensions" = [];
-        "settingsSync.ignoredKeyBindings" = [];
-        "settingsSync.keybindingsPerPlatform" = false;
+    userSettings = {
+      "workbench.startupEditor" = "none";
+      "terminal.integrated.defaultProfile.linux" = "bash";
+      "terminal.integrated.env.linux" = {
+        "KUBECONFIG" = "/home/user/.kube/config";
       };
-      extensions = with pkgs.vscode-extensions; [
-        vscodevim.vim
-      ];
-      keybindings = [
-        {
-          key = "ctrl+t";
-          command = "-workbench.action.quickOpen";
-        }
-        {
-          key = "ctrl+t";
-          command = "kilo-code.openInNewTab";
-        }
-        {
-          key = "ctrl+shift+t";
-          command = "-workbench.action.reopenClosedEditor";
-        }
-        {
-          key = "ctrl+shift+t";
-          command = "cline.openInNewTab";
-        }
-      ];
+      "keyboard.dispatch" = "keyCode";
+      "vim.useSystemClipboard" = true;
+      "editor.lineNumbers" = "relative";
+      "vim.hlsearch" = true;
+      "vim.insertModeKeyBindings" = [];
+      "vim.normalModeKeyBindings" = [];
+      "vim.visualModeKeyBindings" = [];
+      "settingsSync.ignoredSettings" = [];
+      "settingsSync.ignoredExtensions" = [];
+      "settingsSync.ignoredKeyBindings" = [];
+      "settingsSync.keybindingsPerPlatform" = false;
     };
+    extensions = with pkgs.vscode-extensions; [
+      vscodevim.vim
+    ];
+    keybindings = [
+      {
+        key = "ctrl+t";
+        command = "-workbench.action.quickOpen";
+      }
+      {
+        key = "ctrl+t";
+        command = "kilo-code.openInNewTab";
+      }
+      {
+        key = "ctrl+shift+t";
+        command = "-workbench.action.reopenClosedEditor";
+      }
+      {
+        key = "ctrl+shift+t";
+        command = "cline.openInNewTab";
+      }
+    ];
   };
 }
